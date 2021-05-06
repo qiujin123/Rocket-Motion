789a8a1fefa729c849b8a59fb10bd578cd5c2ac2
qiujin123 <qiujinzou123@gmail.com>
2021-05-06 14:35:27 -0400

    tweak delay

--- dev/js/dogMoon.js
+++ dev/js/dogMoon.js
@@ -3,8 +3,8 @@ import { gsap } from "gsap";
 export let dogMoonTL = gsap.timeline();
 
 
-dogMoonTL.to("#Space",{duration:3, scale:1},"moonZoomOut")
-            .to("#moon",{duration:3, scale:1.5, y:400},"moonZoomOut")
-            .to("#rocket",{duration:3, scale:.3, y:-975},"moonZoomOut")
+dogMoonTL.to("#Space",{delay:.5, duration:3, scale:1},"moonZoomOut")
+            .to("#moon",{delay:.5, duration:3, scale:1.5, y:400},"moonZoomOut")
+            .to("#rocket",{delay:.5, duration:3, scale:.3, y:-975},"moonZoomOut")
             .fromTo("#dogMoon",{alpha:0},{alpha:1, duration:3,ease:"none"},"toTheMoon")
             .fromTo("#toTheMoon",{alpha:0},{alpha:1, duration:3,ease:"none"},"toTheMoon")
